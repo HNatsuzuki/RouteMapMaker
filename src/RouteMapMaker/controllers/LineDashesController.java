d9bc03fb5887d243ef4f0db92167a0c369b99330
Natsuzuki <161918455+HNatsuzuki@users.noreply.github.com>
2025-09-13 19:31:25 +0900

    破線パターン変換用メソッド作成

--- src/RouteMapMaker/controllers/LineDashesController.java
+++ src/RouteMapMaker/controllers/LineDashesController.java
@@ -1,6 +1,7 @@
 package RouteMapMaker.controllers;
 
 import java.net.URL;
+import java.text.ParseException;
 import java.util.ResourceBundle;
 
 import RouteMapMaker.factories.AlertFactory;
@@ -96,50 +97,35 @@ public class LineDashesController implements Initializable{
 			}
 		});
 		textField.setOnAction((ActionEvent) ->{
-			String text = textField.getText();
-			//以下テキスト解析
-			String[] split = text.split(",",0);
 			LineDash daw = list.getSelectionModel().getSelectedItem();
-			if(daw == null){
+
+			if (daw == null) {
 				Alert alert = alertFactory.createAlert(AlertType.WARNING,"",ButtonType.CLOSE);
 				alert.getDialogPane().setContentText("項目を選択してください");
 				alert.showAndWait();
-			}else{
-				if(split.length == 0 || split.length == 1){
-					Alert alert = alertFactory.createAlert(AlertType.WARNING,"",ButtonType.CLOSE);
-					alert.getDialogPane().setContentText("破線パターンを表す要素数は2つ以上の必要があります。");
-					alert.showAndWait();
-					setText(daw);
-				}else{
-					double[] da = new double[split.length];
-					boolean error = false;
-					for(int i = 0; i < split.length; i++){
-						try{
-							da[i] = Double.valueOf(split[i]);
-							if(da[i] < 1){
-								Alert alert = alertFactory.createAlert(AlertType.ERROR,"",ButtonType.CLOSE);
-								alert.getDialogPane().setContentText("要素として使用できるのは1以上の半角数字のみです。");
-								alert.showAndWait();
-								error = true;
-								break;
-							}
-						}catch(NumberFormatException e){
-							error = true;
-							Alert alert = alertFactory.createAlert(AlertType.ERROR,"",ButtonType.CLOSE);
-							alert.getDialogPane().setContentText("不適切な文字が使用されています。使用できるのは1以上の半角数字と区切りカンマのみです。");
-							alert.showAndWait();
-						}
-					}
-					if(! error) daw.set(da);
-					setText(daw);
-					gc.clearRect(0, 0, 200, 20);
-					gc.setLineDashes(daw.get());
-					gc.strokeLine(0, 10, 200, 10);
-					list.setItems(null);
-					list.setItems(lineDashes);
-					list.getSelectionModel().select(daw);
-				}
+
+				return;
 			}
+
+			//以下テキスト解析
+			String text = textField.getText();
+
+			try {
+				int index = lineDashes.indexOf(daw);
+				daw = LineDash.parse(text);
+				lineDashes.set(index, daw);
+			} catch (ParseException ex) {
+				Alert alert = alertFactory.createAlert(AlertType. WARNING,ex.getMessage(), ButtonType.CLOSE);
+				alert.showAndWait();
+			}
+
+			setText(daw);
+			gc.clearRect(0, 0, 200, 20);
+			gc.setLineDashes(daw.get());
+			gc.strokeLine(0, 10, 200, 10);
+			list.setItems(null);
+			list.setItems(lineDashes);
+			list.getSelectionModel().select(daw);
 		});
 	}
 	public void setObject(ObservableList<LineDash> lineDashes){
--- src/RouteMapMaker/models/LineDash.java
+++ src/RouteMapMaker/models/LineDash.java
@@ -1,20 +1,49 @@
 package RouteMapMaker.models;
 
+import java.text.ParseException;
+
 /**
  * 破線パターンを表すクラスです。
  */
 public class LineDash {
-	private double[] lineDashPattern;
+    private double[] lineDashPattern;
 
-	public LineDash(double[] pattern){
-		lineDashPattern = pattern;
-	}
+    public LineDash(double[] pattern){
+        lineDashPattern = pattern;
+    }
 
-	public void set(double[] pattern){
-		lineDashPattern = pattern;
-	}
+    public double[] get(){
+        return lineDashPattern;
+    }
 
-	public double[] get(){
-		return lineDashPattern;
-	}
+    /**
+     * 文字列から破線パターンに変換します。
+     *
+     * @param patternText 破線パターンを表す文字列
+     * @return 破線パターン
+     * @throws ParseException 文字列の解析に失敗した場合にスローされます。
+     */
+    public static LineDash parse(String patternText) throws ParseException {
+        String[] splitted = patternText.split(",");
+
+        if (splitted.length < 2) {
+            throw new ParseException("破線パターンを表す要素数は2つ以上の必要があります。", 0);
+        }
+
+        double[] pattern = new double[splitted.length];
+
+        for (int i = 0; i < splitted.length; ++i) {
+            try {
+                pattern[i] = Double.parseDouble(splitted[i]);
+
+                if (pattern[i] < 1) {
+                    throw new ParseException("要素として使用できるのは1以上の半角数字のみです。", i);
+                }
+            } catch (NumberFormatException e) {
+                throw new ParseException("不適切な文字が使用されています。使用できるのは1以上の半角数字と区切りカンマのみです。", i);
+            }
+        }
+
+        return new LineDash(pattern);
+    }
 }
